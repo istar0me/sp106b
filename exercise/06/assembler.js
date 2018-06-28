1da48e8ec896c137dc6128c295e4e981d439d0f8
林柏億 <istar0me@gmail.com>
2018-06-28 15:41:40 +0800

    test

--- exercise/06/assembler.js
+++ exercise/06/assembler.js
@@ -32,7 +32,6 @@ var cTable = {
     "D|A": 0b0010101,
     "D|M": 0b1010101
 }
-console.log(`cTable['D'] = ${cTable['D']}`)
 
 // Destination Table
 var dTable = {
@@ -85,7 +84,7 @@ var symbolTable = {
     "KBD": 24576
 }
 
-var symTop = 16
+var symTop = 16 // 使用者自定義符號的開頭位址
 function addSymbol(symbol) {
     symbolTable[symbol] = symTop;
     symTop++;
@@ -96,7 +95,10 @@ function assemble(asmPath, hackPath) {
     console.log(`【彙編檔 : ${asmPath} 】`)
     var asmFile = fs.readFileSync(asmPath, "utf-8") // 以 UTF-8 的格式讀入檔案
     var splitedAsm = asmFile.split(/\r?\n/) // 拆成一行一行的陣列
-    // var jsonAsm = JSON.stringify(splitedAsm, null, 2) // 轉換為 JSON 格式
+    // printAlignedDash(splitedAsm[0].length)
+    // printLineNum(splitedAsm, 10, 3)
+    var jsonAsm = JSON.stringify(splitedAsm, null, 2) // 轉換為 JSON 格式
+    console.log(jsonAsm)
     checkType(splitedAsm)
     convert(splitedAsm, hackPath)
 }
@@ -108,33 +110,38 @@ function parse(asmPath, i) {
     var code = RegExp.$1.trim() // 將剛擷取的程式碼宣告為 code
     if (code.length == 0) return null // 比對沒有內容的字串
 
-    // 比對 A 指令符號的變數
+    // 比對 A 指令
     else if (code.slice(0, 1) == "@") {
         return { type: "A", argument: code.substring(1).trim() } // 由於第 0 個為 @ 符號，因此用 substring(1) 取出後面的內容
 
-        // 比對 A 指令符號的標籤
+    // 比對 S 指令（符號）
     } else if (code.match(/^\((\w+)\)$/)) {
         return { type: "S", symbol: RegExp.$1 }
 
-        // 比對 C 指令
+    // 比對 C 指令
     } else if (code.match(/^((([AMD]*)=)?([AMD01\+\-\&\|\!]*))(;(\w*))?$/)) {
         return { type: "C", c: RegExp.$4, d: RegExp.$3, j: RegExp.$6 }
 
-        // 錯誤處理
+    // 錯誤處理
     } else {
         throw `Error at line ${i}`
     }
 }
 
 function checkType(asmPath) {
+    console.log("========== type ==========")
     var address = 0
     for (i = 0; i < asmPath.length; i++) {
         var p = parse(asmPath[i], i)
-        if (p == null) continue
+        if (p == null) continue // 如果沒有內容，則跳出
+        
+        // 如果是符號，則去符號表尋找他的位址
         else if (p == "S") {
             console.log(`symbol : ${p.symbol}`)
             symbolTable[p.symbol] = address
             continue
+
+        // 其他則直接印出
         } else {
             console.log("p: %j", p)
         }
@@ -145,43 +152,48 @@ function checkType(asmPath) {
 
 function convert(asmPath, hackPath) {
     console.log("==========convert==========")
-    var ws = fs.createWriteStream(hackPath);
-    ws.once('open', function (fd) {
+    var ws = fs.createWriteStream(hackPath); // 開始寫入工作
+    ws.once('open', function (fd) { // 要寫入的內容
         var address = 0;
         for (var i = 0; i < asmPath.length; i++) {
-            var p = parse(asmPath[i], i);
+            var p = parse(asmPath[i], i); // 獲取每一行程式的 type, argument, symbol, c, d, j 指令
             if (p === null || p.type === "S") continue; // 如果是空白或符號則跳出
-            var code = toCode(p);
+            var code = toCode(p); // 將每一行程式碼轉換成二進位碼
             console.log("%s:%s %s", intToStr(i + 1, 3, 10), intToStr(code, 16, 2), asmPath[i]); // 格式：行數:二進位碼 組合語言
-            ws.write(intToStr(code, 16, 2) + "\n");
+            ws.write(intToStr(code, 16, 2) + "\n"); // 將內容寫入到暫存的檔案
             address++;
         }
-        ws.end();
+        ws.end(); // 結束，將暫存檔寫入真正的檔案
     })
 }
 
+// 轉換為二進位碼
 function toCode(p) {
     var address;
+    // 處理 A 指令
     if (p.type === "A") {
+        // 處理 A 指令的數字
         if (p.argument.match(/^\d+$/)) { // d:digit(數字)，如 @100
             address = parseInt(p.argument); // 將字串轉換成數字(如 "100" 轉換成 100)
-        } else { // 如 @label, @variable
+        
+        // 處理 Symbol 的標籤與變數
+        } else {
             address = symbolTable[p.arg]; // 取出它的地址
+
+            // 如果是沒見過的符號，則將它加入符號表
             if (typeof address === 'undefined') {
                 address = symTop;
                 addSymbol(p.argument, address);
             }
         }
         return address;
-    } else { // if (p.type === "C")
+
+    // 處理 C 指令
+    } else {
         var d = dTable[p.d];
-        var cx = cTable[p.c];
+        var c = cTable[p.c];
         var j = jTable[p.j];
-        // console.log(`111 << 13 = ${111 << 13}`)
-        // console.log(`cx << 6 = ${cx << 6}`)
-        // console.log(`d << 3 = ${d << 3}`)
-        // console.log(`j = ${j}`)
-        return 111 << 13 | cx << 6 | d << 3 | j;
+        return 0b111 << 13 | c << 6 | d << 3 | j;
     }
 }
 
@@ -193,19 +205,19 @@ function printAlignedDash(length) {
     console.log(dash)
 }
 
-// printAlignedDash(splitedAsm[0].length)
-// function printLineNum(content, radix, length) {
-//     // for (i=0;i<content.length;i++) {
-//     let lineNum = i.toString(radix)+""
-//     while(content.length < length) {
-//         lineNum = "0" + lineNum
-//     }
-//     return lineNum
-//     // }
-// }
+function printLineNum(content, radix, length) {
+    let linNum
+    for (i = 0; i < content.length; i++) {
+        lineNum = i.toString(radix)
+        while (content.length < length) {
+            lineNum = "0" + lineNum
+        }
+        console.log(lineNum)
+    }
+}
 
 function intToStr(num, size, radix) {
-    var output = num.toString(radix) + "";
+    var output = num.toString(radix);
     while (output.length < size) {
         output = "0" + output;
     }
